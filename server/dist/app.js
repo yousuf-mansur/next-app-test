fd461dfd19054d99d38e5e3ec1b5d2c043794802
Md. Yousuf Mansur <mansurmdyousuf@gmail.com>
2025-01-11 03:14:57 +0600

    Corrected

---
+++ server/dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Load environment variables
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// Middleware
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running from app.ts!');
+});
+// Export the app
+exports.default = app;
